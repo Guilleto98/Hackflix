d3943915c1fb686e2aac9570c80c283bb01443b3
Martin Hernandez <martin.hernandez.g@gmail.com>
2022-10-26 10:40:27 -0300

    SeAcomodoAxios

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,19 +8,33 @@ function MovieList({ rating }) {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
 
+  /*  */
+
   const getMovies = async () => {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=d31021610512441435829706f00ea294&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`
-    );
+    const response = await axios({
+      url: `https://api.themoviedb.org/3/discover/movie?`,
+      method: "GET",
+      params: {
+        api_key: `d31021610512441435829706f00ea294`,
+        language: `en-US`,
+        sort_by: `popularity.desc`,
+        include_adult: false,
+        include_video: false,
+        page: page,
+        "vote_average.gte": rating,
+        with_watch_monetization_types: `flatrate`,
+      },
+    });
 
     setMovies([...movies, ...response.data.results]);
     setPage(page + 1);
-    console.log(page);
+    console.log(rating);
   };
 
   useEffect(() => {
     getMovies();
-  });
+  }, []); // eslint-disable-line
+
   /* 
   let filteredMovies = movies.filter(
     (movie) => movie.vote_average >= (rating - 1) * 2
