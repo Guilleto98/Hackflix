35293e2ebb8aa55fd98238b51f779c397c2e15b3
santiagoG10 <112738532+santiagoG10@users.noreply.github.com>
2022-10-27 15:18:12 -0300

    adding route to button Hackflix and Home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Button from "react-bootstrap/esm/Button";
-import { FaFilm} from 'react-icons/fa';
-import {Link} from 'react-router-dom'
-
-
+import { FaFilm } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <nav className="navbar d-flex justify-content-around">
         <div>
-          
-          <a className="navbar-brand link-color fw-bold fs-3" href="back">
-          <FaFilm/> Hackflix 
+          <a className="navbar-brand link-color fw-bold fs-3" href="/">
+            <FaFilm /> Hackflix
           </a>
         </div>
         <div className="d-flex align-items-center">
-        <a className="navbar-brand link-color fw-bold fs-3" href="back">
-          Home
-        </a>
-        <Button variant="outline-danger h-25"><Link to='/search' className="text-decoration-none text-white">Search</Link></Button>
+          <a className="navbar-brand link-color fw-bold fs-3" href="/">
+            Home
+          </a>
+          <Button variant="outline-danger h-25">
+            <Link to="/search" className="text-decoration-none text-white">
+              Search
+            </Link>
+          </Button>
         </div>
       </nav>
       <div className="welcome">
